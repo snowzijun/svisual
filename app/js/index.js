aceb0699541dd94b7dfe3ae0a6559bd23cade044
lee <lee@lee-PC>
2017-07-24 22:09:54 +0800

    阿萨德

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,29 @@
 require(['app/common.js'],function(){
 	require(['zepto','cssloader!css/index'],function($){
-//		alert(1);
+		var initEvent = function(){
+			mui(".footer-center").on("tap","#msg-sound",function(event){
+				if (window.plus) {
+					document.body.classList.add(CLASS_FOCUSIN);
+					plus.speech.startRecognize({
+						engine: 'iFly',
+						timeout:20
+					}, function(s) {
+						alert(s);
+						plus.speech.stopRecognize();
+					}, function(e) {
+						document.body.classList.remove(CLASS_FOCUSIN);
+					});
+				} else {
+					alert('only for 5+');
+				}
+				event.preventDefault();
+			});
+		}
+		
+		var init = function(){
+			initEvent();
+		}
+		
+		init();
 	})
 })
