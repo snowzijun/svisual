0f655399d24a43faf0298da648c27def37536674
lujt <lujt@lenovo-PC>
2017-08-09 21:44:22 +0800

    语音输入

--- app/js/index.js
+++ app/js/index.js
@@ -2,25 +2,19 @@ require(['app/common.js'],function(){
 	require(['zepto','cssloader!css/index'],function($){
 		var initEvent = function(){
 			mui(".footer-center").on("tap","#msg-sound",function(event){
-				if (window.plus) {
-					document.body.classList.add(CLASS_FOCUSIN);
-					plus.speech.startRecognize({
-						engine: 'iFly',
-						timeout:20
-					}, function(s) {
-						alert(s);
-						plus.speech.stopRecognize();
-					}, function(e) {
-						document.body.classList.remove(CLASS_FOCUSIN);
-					});
-				} else {
-					alert('only for 5+');
-				}
+				var options = {};
+				options.engine = 'iFly';
+				options.continue = true;
+				options.timeout = 20000;
+				options.userInterface = false;
+				plus.speech.startRecognize(options, function(s) {
+					alert(s);
+				}, function(e) {
+					
+				});
 				event.preventDefault();
 			});
-			mui(".footer-center").on("hold","#msg-sound",function(event){
-				console.log("a");
-			});
+			
 			//个人中心
 			mui("body").on("tap",".user-center",function(event){
 				mui.openWindow({
--- app/js/usercenter.js
+++ app/js/usercenter.js
@@ -2,20 +2,16 @@ require(['app/common.js'],function(){
 	require(['zepto','cssloader!css/usercenter'],function($){
 		var initEvent = function(){
 			mui(".footer-center").on("tap","#msg-sound",function(event){
-				if (window.plus) {
-					document.body.classList.add(CLASS_FOCUSIN);
-					plus.speech.startRecognize({
-						engine: 'iFly',
-						timeout:20
-					}, function(s) {
-						alert(s);
-						plus.speech.stopRecognize();
-					}, function(e) {
-						document.body.classList.remove(CLASS_FOCUSIN);
-					});
-				} else {
-					alert('only for 5+');
-				}
+				var options = {};
+				options.engine = 'iFly';
+				options.continue = true;
+				options.timeout = 20000;
+				options.userInterface = false;
+				plus.speech.startRecognize(options, function(s) {
+					alert(s);
+				}, function(e) {
+					
+				});
 				event.preventDefault();
 			});
 			mui(".footer-center").on("hold","#msg-sound",function(event){
